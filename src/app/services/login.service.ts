98f8821b82789f18264c6614f2a852e84b9d547d
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-06-02 14:56:00 -0300

    Alteração de verificação se está logado no serviço

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { mockUsers as mock } from '../mocks/usersMocks';
 import { SnackbarService } from './snackbar.service';
 import { Router } from '@angular/router';
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
 export class LoginService {
   // This service is a mock, and needs to be changed in the future to work correctly.
   mockUsers = mock;
+  isLogged = new EventEmitter<boolean>();
 
   constructor(private _snackbar: SnackbarService, private router: Router) {}
 
@@ -18,25 +19,19 @@ export class LoginService {
     if (user && user.password == password) {
       localStorage.setItem('token', user.token);
       this.router.navigate(['home']);
+      this.isLogged.emit(true);
     } else {
       this._snackbar.openSnackBar(
         'Email ou senhas passados incorretos! Tente novamente'
       );
+      this.isLogged.emit(false);
     }
   }
 
-  isLogged() {
-    let token = localStorage.getItem('token');
-
-    if (token) {
-      return true;
-    }
-
-    return false;
-  }
 
   logout() {
     localStorage.removeItem('token');
     this.router.navigate(['login']);
+    this.isLogged.emit(false);
   }
 }
