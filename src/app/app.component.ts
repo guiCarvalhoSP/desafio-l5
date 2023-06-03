fe30ae9ee1a0eb7176b9156ba63fec8103dc0eec
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-06-03 18:31:00 -0300

    Botão para voltar ao topo da página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { LoginService } from './services/login.service';
 
 @Component({
@@ -9,11 +9,32 @@ import { LoginService } from './services/login.service';
 export class AppComponent {
   isLogged: boolean = false;
 
+  isShowButton: boolean = false;
+  startShowingButton = 250;
+
   constructor(private loginService: LoginService) {}
 
   ngOnInit() {
     this.loginService.isLogged.subscribe((value) => {
       this.isLogged = value;
-    })
+    });
+  }
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollPosition >= this.startShowingButton) {
+      this.isShowButton = true;
+    } else {
+      this.isShowButton = false;
+    }
+  }
+
+  goToTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
   }
 }
